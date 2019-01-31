245e053d79769c7350b7ec0057f362347e82846b
IronMan61693 <dominic.pontious@gmail.com>
2019-01-31 14:28:26 -0700

    I realized I had my prompt commented out, changed that

--- lab2_scripts.sh
+++ lab2_scripts.sh
@@ -4,13 +4,13 @@
 # Lab 2 problems
 
 # Here are the accepted paramaters for the lab
-# echo "Please enter the file name you would like to work with : "
-# read fileName
-# echo "Now enter the regular expression which we would like to use : "
-# read regExPrompted
+echo "Please enter the file name you would like to work with : "
+read fileName
+echo "Now enter the regular expression which we would like to use : "
+read regExPrompted
 
-# # Here we are printing the results of the prompt
-# grep -E $regExPrompted $fileName
+# Here we are printing the results of the prompt
+grep -E $regExPrompted $fileName
 
 # Here we are using the hardcode grep commands
 
@@ -36,10 +36,10 @@ grep -Ec "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}" regex_practice.txt
 # List all phone numbers in the 303 area code and store in phone_results.txt
 grep -E "303-[0-9]{3}-[0-9]{4}" regex_practice.txt > phone_results.txt
 
-# List all emails NOT from geocities.com and store the results in email_results.txt
+# List all emails from geocities.com and store the results in email_results.txt
 grep -E "[A-Za-z0-9._%+-]+@geocities.com" regex_practice.txt > email_results.txt
 
-# Command-line regular expresion
+# Command-line regular expresion and store results in command_results.txt
 grep $1 regex_practice.txt > command_results.txt
 
 
